775fa233174b91bde98ac4e7b5797b49ad356e83
Brady Cartmell <bradycartmell+localgit@gmail.com>
2015-09-09 16:11:11 -0500

    changes port to 443 for secure connections

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var server = require('./server/server.js');
 var portNumber;
 
-portNumber = (process.env.NODE_ENV === 'production' ? 80 : 3000);
+portNumber = (process.env.NODE_ENV === 'production' ? 443 : 3000);
 console.log('Environment: '+ process.env.NODE_ENV);
 console.log('port: '+ portNumber);
 server.server.start(portNumber);
