0a05be92959173f0b4b4a020ad052bed267ee8d0
Brady Cartmell <bradycartmell+git@gmail.com>
2016-01-23 15:31:17 -0600

    attempt to fix curtain size on ios devices

--- resume.js
+++ resume.js
@@ -84,6 +84,12 @@
           hideElement(self.element);
         }
       });
+
+      Curtain.prototype.setHieght.call(this);
+      window.addEventListener('resize', function(){
+        self.setHieght.call(this);
+      });
+
       return this;
     };
     Curtain.prototype = {
@@ -96,6 +102,9 @@
         for (i=0; i<this.modals.length; i++) {
           hideElement(this.modals[i].element);
         }
+      },
+      setHieght: function() {
+        this.element.style.minHeight = helpers.getViewHeight();
       }
     };
 
