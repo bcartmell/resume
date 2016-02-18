43ef3e4da31f4c689251870e9d4eec72a49c4e54
Brady Cartmell <bradycartmell+git@gmail.com>
2016-02-18 11:35:23 -0600

    pass default page to server as an argument instead of hard-coding it

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 'use strict'
 var server = require('./okie-serv/server.js').server;
 var portNumber = 8020;
-var defaultPage = 'resume.html';
-server.start(portNumber);
+server.start(portNumber, 'resume.html');
